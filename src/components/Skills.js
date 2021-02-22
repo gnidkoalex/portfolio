e2dfa1b419daaaa76cd0c53541a83c0918cc8656
Alex <gnidkoalex@gmail.com>
2021-02-22 19:50:10 +0200

    skills css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import "./Skills.css"
 
 function Skills() {
     return (
         <>
             <div>
                 <h1>Skills Component</h1>
-            </div>   
+            </div>  
+            <div className="neon-container">
+                <a className="react">
+                React
+                </a>
+                <a className="js">
+                js
+                </a>
+                <a className="css">
+                css
+                </a>
+                <a className="PHP">
+                PHP
+                </a>
+                <a className="node">
+                node JS
+                </a>
+            </div> 
+            
         </>
     )
 }
