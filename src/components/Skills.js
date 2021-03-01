b8fe9b0907010884011a112da20258f224f8d3fc
Alex <gnidkoalex@gmail.com>
2021-03-01 11:38:44 +0200

    skills ready

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,11 +2,14 @@ import React from 'react'
 import "./Skills.css"
 
 function Skills() {
+    const mouseOnLanguage= (e)=>{
+        e.target.innerText="Hebrew-native , English-fluent , Russian-fluent"
+    }
+    const mouseLeftLanguage= (e)=>{
+        e.target.innerText="Languages"
+    }
     return (
-        <>
-            <div>
-                <h1>Skills Component</h1>
-            </div>  
+        <> 
             <div className="neon-container">
                 <a className="react">
                 React
@@ -23,6 +26,29 @@ function Skills() {
                 <a className="node">
                 node JS
                 </a>
+                <a className="redux">
+                Redux
+                </a>
+                <a className="jQuery">
+                jQuery
+                </a>
+                <a className="html">
+                HTML
+                </a>
+                <a className="express">
+                Express
+                </a>
+                <a className="mysql">
+                MySQL
+                </a>
+                <a className="mongo">
+                mongo DB
+                </a>
+                <a className="leng" onMouseOver={mouseOnLanguage} onMouseOut={mouseLeftLanguage} >
+                Languages
+            
+                </a>
+               
             </div> 
             
         </>
