765d5740b7f9570168e3fa83e7171f310e2bceb9
Alex <gnidkoalex@gmail.com>
2021-03-21 19:09:52 +0200

    contact me ready

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,6 +8,7 @@ import './Contact.css'
 export default function Contact() {
 
     function sendEmail(e) {
+        console.log("well done")
         e.preventDefault();
 
         emailjs.sendForm('service_Gmail', 'template_nafzadu', e.target, 'user_ADGZdh6SWSzYwXliz1LnR')
@@ -20,34 +21,77 @@ export default function Contact() {
     }
 
     return (
+        // <div className="contactContainer">
+        //     <div className="login-box">
+        //         <h2>Contact Me</h2>
+        //         <form onSubmit={sendEmail}>
+        //             <div class="user-box">
+        //                 <input type="text" name="name" required="" />
+        //                 <label>Name / Company</label>
+        //             </div>
+        //             <div class="user-box">
+        //                 <input type="text" name="email" required="" />
+        //                 <label>Email</label>
+        //             </div>
+        //             <div class="user-box">
+        //                 <label>Message</label>
+        //                 <textarea name="message" />
+        //             </div>
+        //             <a href="#" onClick={sendEmail}>
+        //             <input type="submit" value="Send" />
+        //                 <span></span>
+        //                 <span></span>
+        //                 <span></span>
+        //                 <span></span>
+        //                 Send email
+        //             </a>
+        //         </form>
+        //     </div>
+        // </div>
         <div className="contactContainer">
-            <div class="login-box">
-                <h2>Login</h2>
-                <form>
-                    <div class="user-box">
-                        <input type="text" name="" required="" />
-                        <label>Username</label>
-                    </div>
-                    <div class="user-box">
-                        <input type="password" name="" required="" />
-                        <label>Password</label>
-                    </div>
-                    <div class="user-box">
-                        <input type="password" name="" required="" />
-                        <label>Password</label>
-                    </div>
-                    <a href="#">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        Submit
-                    </a>
-                </form>
+        <div  className="login-box">
+                <h2>Contact Me</h2>
+        <form className="contact-form" onSubmit={sendEmail}>
+            <div className="user-box"> 
+            <input type="text" name="name" />
+            <label>Name</label>
             </div>
+            <div className="user-box"> 
+            <input type="text" name="email" />
+            <label>email</label>
+            </div>
+            <div className="user-box"> 
+            <label>message</label>
+            <textarea name="message" cols="30" rows="10"/>
+            
+            </div>
+        {/* <input type="submit" value="Send" /> */}
+        <button type="submit" >
+                        <span></span>
+                       <span></span>
+                       send
+                        <span></span>
+                        <span></span>
+                     </button>
+        </form>
         </div>
+    
+    </div>
 
     );
+    <div className="contactContainer">
+        <div  className="login-box">
+                <h2>Contact Me</h2>
+        <form className="contact-form" onSubmit={sendEmail}>
+            <div className="user-box"> 
+            <input type="hidden" name="name" />
+            <label>Name</label>
+            </div>
+        <input type="submit" value="Send" />
+        </form>
+        </div>
+    
+    </div>
 
 
 }
