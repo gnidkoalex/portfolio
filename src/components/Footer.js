bd400a2595314966f7efcca30b6ca4b9c6826ae7
Alex <gnidkoalex@gmail.com>
2021-03-29 18:29:07 +0300

    footer ready

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,32 +3,18 @@ import './Footer.css'
 
 function Footer() {
     return (
-        <div className="mainFooter">
-            <div className="footerContainer">
-                <div className="col">
-                    <ul className="item">
-                        <span>letters</span>
-                        <li>aaaa</li>
-                        <li>bbbb</li>
-                        <li>cccc</li>
-                    </ul>
-                </div>
-                <div className="col">
-                    <ul className="item">
-                        <span>numbers</span>
-                        <li>1111</li>
-                        <li>2222</li>
-                        <li>3333</li>
-                    </ul>
-                </div>
-                <div className="col">
-                    <ul className="item">
-                        <span>multi</span>
-                        <li>11aa</li>
-                        <li>bb22</li>
-                        <li>3c3c</li>
-                    </ul>
-                </div>
+        <div className="footerContainer">
+            {/* <h3> website by alex gnidko</h3> */}
+            <ul className="socials">
+                <li><a href="#" className="fab fa-facebook"></a></li>
+                <li><a href="#" className="fab fa-instagram"></a></li>
+                <li><a href="#" className="fab fa-linkedin"></a></li>
+                <li><a href="#" className="fab fa-github"></a></li>
+            </ul>
+            
+            <div className="footerBottom">
+                <p>copyright &copy; 2021 . designed by <span>alex gnidko</span></p>
+
             </div>
         </div>
     )
